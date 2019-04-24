e03c0c872ca90c2c2c36c2cd809a8492fa106ae4
diogopaza <diogopazacvel@gmail.com>
2019-04-24 14:59:05 -0300

    testing

--- ponteiros.go
+++ ponteiros.go
@@ -11,6 +11,15 @@ func main(){
 
 	nome:= "lucas magnum"
 	fmt.Printf("%v\n", nome)
+	
+
+	var ponteiro *string
+
+	ponteiro = &nome
 	fmt.Println("Endereço de memória: ", &nome)
+	fmt.Println("Endereço no ponteiro: ", ponteiro)
+
+	fmt.Println("Endereço no endereço de memória: ", *ponteiro)
+
 
 }
